2a517655925b70c6be81f24c024570f2000bef8a
lichangsheng <cli@corilead.com>
2022-07-27 18:59:19 +0800

    init

---
+++ config/routes.ts
@@ -0,0 +1,9 @@
+﻿export default [
+  { path: '/welcome', name: 'welcome', icon: 'smile', component: './Welcome' },
+  { path: '/app1', name: 'app1', microApp: 'app1', microAppProps: { errorBoundary: () => '错啦' } },
+  { path: '/app2', name: 'app2', microApp: 'app2', microAppProps: { errorBoundary: () => '错啦' } },
+  { path: '/', redirect: '/app1' },
+  {
+    component: './404',
+  },
+];
